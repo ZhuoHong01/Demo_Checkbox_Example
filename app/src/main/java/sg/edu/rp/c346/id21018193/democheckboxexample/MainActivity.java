0dbc473156b9b7713f87ee490af0df62a47dbc46
lee zhuo hong <21018193@myrp.edu.sg>
2022-05-11 11:08:08 +0800

    .

--- app/src/main/java/sg/edu/rp/c346/id21018193/democheckboxexample/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/democheckboxexample/MainActivity.java
@@ -3,12 +3,23 @@ package sg.edu.rp.c346.id21018193.democheckboxexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox cbEnabled;
+    Button btnCheck;
+    TextView tvShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cbEnabled = findViewById(R.id.checkBoxDiscount);
+        btnCheck = findViewById(R.id.buttonCheck);
+        tvShow = findViewById(R.id.textView);
     }
 }
