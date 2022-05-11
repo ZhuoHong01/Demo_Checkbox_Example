bec0fcceaedee725eafd19edeb8c907541870855
lee zhuo hong <21018193@myrp.edu.sg>
2022-05-11 11:10:50 +0800

    .

--- app/src/main/java/sg/edu/rp/c346/id21018193/democheckboxexample/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/democheckboxexample/MainActivity.java
@@ -21,5 +21,8 @@ public class MainActivity extends AppCompatActivity {
         cbEnabled = findViewById(R.id.checkBoxDiscount);
         btnCheck = findViewById(R.id.buttonCheck);
         tvShow = findViewById(R.id.textView);
+
     }
+
+
 }
